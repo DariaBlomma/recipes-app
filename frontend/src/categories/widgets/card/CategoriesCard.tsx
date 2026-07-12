a7797917abb34c95012ae308275eb2c899cec1db
Daria Kozina <kozina.flowers@yandex.ru>
2026-07-12 10:03:34 +0300

    front: categories delete hidden

--- frontend/src/categories/widgets/card/CategoriesCard.tsx
+++ frontend/src/categories/widgets/card/CategoriesCard.tsx
@@ -4,15 +4,21 @@ import type {CategorySchema} from "@/categories/types";
 
 export function CategoriesCard ({ name, id } : CategorySchema) {
     const navigate = useNavigate();
+
     const onEditClick = () => {
         navigate({to: `/categories/edit/${id}`})
     }
+
+
     return (
         <div className={styles.card}>
             <span className={styles.title}>{ name }</span>
             <div className={styles.actionsBtns}>
                 <div className={[styles.actionBtn, styles.edit].join(' ')} onClick={onEditClick}>Edit</div>
-                <div className={[styles.actionBtn, styles.delete].join(' ')}>Delete</div>
+                {/*todo: do later, categoryId will be optional, change recipes filter by CatoguryId - showAll and show Without Cateogry*/}
+                {/*<div className={[styles.actionBtn, styles.delete].join(' ')} onClick={onDeleteClick}>*/}
+                {/*    Delete*/}
+                {/*</div>*/}
                 {/*todo: implement later*/}
                 {/*<div className={[styles.actionBtn, styles.view].join(' ')}>View</div>*/}
             </div>
