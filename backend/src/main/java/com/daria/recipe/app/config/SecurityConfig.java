cd48f814f6941274db4e8ad404d236a25e02776e
Daria Kozina <kozina.flowers@yandex.ru>
2026-07-14 11:35:26 +0300

    back: fixing cors

--- backend/src/main/java/com/daria/recipe/app/config/SecurityConfig.java
+++ backend/src/main/java/com/daria/recipe/app/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
